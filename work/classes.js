0cd49c332d8b1cb699246e07b4cc981f7ca448ee
elinaoikonomaki <elinaoikonomaki@gmail.com>
2024-04-29 10:15:21 -0400

    added back personal-website to url

--- work/classes.js
+++ work/classes.js
@@ -7,14 +7,14 @@ export class Card {
       this.title = title;
       this.titleclass = titleclass;
       this.classname = classname;
-      this.imgUrl = `${imgUrl}`; //`/Personal-website${imgUrl}`;
+      this.imgUrl = `/Personal-website${imgUrl}`;
       this.tags = tags;
       this.types = types;
       this.year = year;
       this.subtitle = subtitle;
       this.paragraph = paragraph;
-      this.videoUrl = `${videoUrl}`;
-      this.bwUrl = `${bwUrl}`;
+      this.videoUrl = `/Personal-website${videoUrl}`;
+      this.bwUrl = `/Personal-website${bwUrl}`;
       this.div = document.createElement('div');
       this.div.id = id;
       this.div.className = classname;
