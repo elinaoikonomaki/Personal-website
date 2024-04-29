c831cd7ad1e381e2a97b17166333d7125769abce
elinaoikonomaki <elinaoikonomaki@gmail.com>
2024-04-29 02:54:19 -0400

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite';
+import path from 'path'; // Import path module from Node.js
 
 export default defineConfig({
-  base: '/Personal-website/'
+  base: '/Personal-website/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'), // path for the main entry
+        about: path.resolve(__dirname, 'about/index.html'), // path for the about page
+        work: path.resolve(__dirname, 'work/index.html') // path for the work page
+      }
+    }
+  }
 });
